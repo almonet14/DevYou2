7bf1896aa902d8bc6e8aff227cfb1908037d81a2
almonet14 <67608997+almonet14@users.noreply.github.com>
2020-07-27 15:24:25 -0400

    Add files via upload

---
+++ DevYou2RoomAreaCalc/DevYou2RoomAreaCalc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DevYou2RoomAreaCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string run = "y";
+
+            while (run == "y")
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter the length of your room.");
+                int length = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the width of your room.");
+                int width = Convert.ToInt32(Console.ReadLine());
+
+                int area = length * width;
+                int perimeter = (2 * length) + (2 * width);
+
+                Console.WriteLine($"The area of your room is {area}");
+                Console.WriteLine($"The perimeter of your room is {perimeter}");
+                Console.WriteLine();
+                Console.WriteLine("Would you like to calculate another room?  Enter 'y' if yes.");
+                run = Console.ReadLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
